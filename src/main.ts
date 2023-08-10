ab787250163dc7e27995583f648da26c301f6acf
Mateo CONDAT <mateo.condat@utt.fr>
2023-08-10 14:55:51 +0200

    Auto-generated commit [ci skip]

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import { GlobalVariablesService } from 'src/app/components/global-variables.service';
-import { Subscription } from 'rxjs';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
